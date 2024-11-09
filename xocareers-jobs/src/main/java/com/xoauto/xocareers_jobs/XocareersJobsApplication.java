b6144f6c0195adec5da0786588aca22e7f95873e
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2024-11-09 07:59:12 +0100

    jobs microservices init

---
+++ xocareers-jobs/src/main/java/com/xoauto/xocareers_jobs/XocareersJobsApplication.java
@@ -0,0 +1,13 @@
+package com.xoauto.xocareers_jobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XocareersJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XocareersJobsApplication.class, args);
+	}
+
+}
