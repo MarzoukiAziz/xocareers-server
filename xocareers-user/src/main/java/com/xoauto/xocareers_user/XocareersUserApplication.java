9f77835d43319e24577eef3b561d373b5f1acb61
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2024-11-09 06:02:05 +0100

    user microservice init

---
+++ xocareers-user/src/main/java/com/xoauto/xocareers_user/XocareersUserApplication.java
@@ -0,0 +1,13 @@
+package com.xoauto.xocareers_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XocareersUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XocareersUserApplication.class, args);
+	}
+
+}
---
+++ xocareers-user/src/test/java/com/xoauto/xocareers_user/XocareersUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xoauto.xocareers_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XocareersUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
